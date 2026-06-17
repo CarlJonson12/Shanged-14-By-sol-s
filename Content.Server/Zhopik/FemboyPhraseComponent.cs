4318ab87d95d76329bdf9b1780dba109e5302e56
ZhoporezRus <flowwww32@gmail.com>
2026-06-17 23:21:40 +0700

    Насрал

--- Content.Server/Zhopik/FemboyPhraseComponent.cs
+++ Content.Server/Zhopik/FemboyPhraseComponent.cs
@@ -1,5 +1,11 @@
 using Content.Server.Zhopik.FemboyPhrase.Components;
+using Content.Shared.Dataset;
+using Robust.Shared.Prototypes;
 namespace Content.Server.Zhopik.FemboyPhrase.Components;
 
 [RegisterComponent]
-public sealed partial class FemboyPhraseComponent : Component;
+public sealed partial class FemboyPhraseComponent : Component
+{
+    [DataField]
+    public ProtoId<DatasetPrototype> Dataset;
+}
